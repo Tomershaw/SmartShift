4b4338c187bd038293960f849899c8c90765b268
Tomershaw <125479370+Tomershaw@users.noreply.github.com>
2025-06-20 13:37:31 +0300

    Fix namespace for RefreshTokenCommand

--- SmartShift.Application/Authentication/RefreshToken/RefreshTokenCommend.cs
+++
@@ -1,9 +0,0 @@
-using MediatR;
-using SmartShift.Application.Authentication.Refresh;
-
-public class RefreshTokenCommand : IRequest<RefreshTokenResult>
-{
-    public string IpAddress { get; set; } = string.Empty; 
-     public string RefreshToken { get; set; } = string.Empty;
-     
-}

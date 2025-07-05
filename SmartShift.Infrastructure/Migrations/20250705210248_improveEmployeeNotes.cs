76bbc9158182a84b5f5600492a7da4c3ffa90690
Tomershaw <shaw.tomer@gmail.com>
2025-07-06 00:07:18 +0300

    Enhance Employee model by adding 'AdminNotes' and 'EmployeeNotes' properties for improved clarity and separation of concerns. Remove the 'Notes' and 'WorkTypes' properties to streamline the model. Update database context and migrations accordingly.

---
+++ SmartShift.Infrastructure/Migrations/20250705210248_improveEmployeeNotes.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartShift.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class improveEmployeeNotes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "WorkTypes",
+                table: "Employees",
+                newName: "EmployeeNotes");
+
+            migrationBuilder.RenameColumn(
+                name: "Notes",
+                table: "Employees",
+                newName: "AdminNotes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EmployeeNotes",
+                table: "Employees",
+                newName: "WorkTypes");
+
+            migrationBuilder.RenameColumn(
+                name: "AdminNotes",
+                table: "Employees",
+                newName: "Notes");
+        }
+    }
+}
